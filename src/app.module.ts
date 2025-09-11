21e10eacfe2b829a8bbeab9e58eac9603ad9a10a
Alessandrror <ssandrorivas@gmail.com>
2025-09-11 09:28:51 -0600

    add: nest config with openai and gemini modules into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { OpenaiModule } from './openai/openai.module';
+import { GeminiModule } from './gemini/gemini.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), OpenaiModule, GeminiModule],
   controllers: [AppController],
   providers: [AppService],
 })
