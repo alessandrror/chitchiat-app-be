ae9e8509344b1ef37913150cd243ec6513c17f33
Alessandrror <ssandrorivas@gmail.com>
2025-09-11 09:38:55 -0600

    remove: openai module from app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { OpenaiModule } from './openai/openai.module';
 import { GeminiModule } from './gemini/gemini.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), OpenaiModule, GeminiModule],
+  imports: [ConfigModule.forRoot(), GeminiModule],
   controllers: [AppController],
   providers: [AppService],
 })
