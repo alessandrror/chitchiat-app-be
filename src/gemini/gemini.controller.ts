38e9e16d85a473ad63f3ab444ce56657d7e61386
Alessandrror <ssandrorivas@gmail.com>
2025-09-12 02:43:17 -0600

    add: generate stream content using sse

--- src/gemini/gemini.controller.ts
+++ src/gemini/gemini.controller.ts
@@ -1,6 +1,7 @@
+import { type Response } from 'express';
 import { GeminiService } from './gemini.service';
 import { ApiTags, ApiBody } from '@nestjs/swagger';
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Res, Sse } from '@nestjs/common';
 import { CreateGenerateContentRequest } from './dto/create-generate-content.request';
 
 @ApiTags('Gemini')
@@ -18,5 +19,43 @@ export class GeminiController {
     return this.geminiService.createGenerateContent({
       data: body.contents[0],
     });
+  }
+
+  @Post()
+  @Sse('streamContent')
+  @ApiBody({
+    type: CreateGenerateContentRequest,
+    description: 'The content to generate',
+    required: true,
+  })
+  async createGenerateContentStream(
+    @Res() res: Response,
+    @Body() body: CreateGenerateContentRequest,
+  ) {
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+    res.flushHeaders();
+
+    try {
+      for await (const chunk of await this.geminiService.createGenerateContentStream(
+        {
+          data: body.contents[0],
+        },
+      )) {
+        if (chunk && chunk.content) {
+          const message = JSON.stringify({ content: chunk.content });
+          for (const line of message.split('\n\n')) {
+            res.write(`data: ${line}\n\n`);
+          }
+        }
+      }
+    } catch (error) {
+      console.error(error);
+      res.write(`data: ${error}\n\n`);
+    } finally {
+      res.write('event: close\n\n');
+      res.end('data: {"DONE"}\n\n');
+    }
   }
 }
